a78a59435c4c016bde1e746bea09453d14689708
Dhruv Sagar <53065041+dhruvel@users.noreply.github.com>
2025-02-16 05:54:05 -0500

    Update install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -4,26 +4,6 @@
 sudo apt update -y
 
 # Install Docker
-if curl -fsSL https://get.docker.com | sh; then
-    echo "Docker installed successfully."
-else
-    echo "Docker installation failed." >&2
-    exit 1
-fi
-
-# Add user to Docker group
-if sudo usermod -aG docker $USER; then
-    echo "User added to Docker group successfully."
-else
-    echo "Failed to add user to Docker group." >&2
-    exit 1
-fi
-
-# Prompt for system restart
-read -p "Docker installation completed. Do you want to restart the system now? (y/n): " choice
-if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
-    echo "Restarting system to apply changes..."
-    sudo shutdown -r now
-else
-    echo "Please restart the system manually to apply changes."
-fi
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
+echo "Please logoff/reboot before running docker."
