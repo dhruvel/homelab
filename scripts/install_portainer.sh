764aea0580df97a4c3439aeec450539d076af06a
Dhruv Sagar <53065041+dhruvel@users.noreply.github.com>
2025-02-23 03:32:35 -0500

    Update install_portainer.sh

--- scripts/install_portainer.sh
+++ scripts/install_portainer.sh
@@ -2,5 +2,5 @@
 
 cd
 sudo mkdir -p /portainer/Files/AppData/Config/portainer
-sudo docker pull portainer/portainer-ce:latest
-sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /portainer/Files/AppData/Config/portainer:/data portainer/portainer-ce:latest
+sudo docker pull portainer/portainer-ce:2.21.3
+sudo docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v /portainer/Files/AppData/Config/portainer:/data portainer/portainer-ce:2.21.3
